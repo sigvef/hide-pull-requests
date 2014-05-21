90afbe5ff218106c89ab18f97d8a514fb04e0c92
Stian Jensen <me@stianj.com>
2014-05-21 11:52:42 +0200

    Scope pull-request hiding to issues-list

--- hide-pull-requests.js
+++ hide-pull-requests.js
@@ -1,4 +1,4 @@
-var pullRequestIcons = document.querySelectorAll('.octicon-git-pull-request');
+var pullRequestIcons = document.querySelectorAll('.issues-list .octicon-git-pull-request');
 for(var i=0;i<pullRequestIcons.length;i++){
     pullRequestIcons[i].parentElement.parentElement.style.display = 'none';
 }
